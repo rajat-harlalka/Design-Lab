effbb32999014a82f797caf829022411302d4f94
Rajat Harlalka <rajat.harlalka2010@gmail.com>
2018-04-22 23:45:34 +0530

    Add files via upload

---
+++ recv2final.ino
@@ -0,0 +1,40 @@
+#include <RH_ASK.h>
+#include <SPI.h> // Not actualy used but needed to compile
+
+RH_ASK driver;
+
+int data1 = 0;
+int data2 = 0;
+int data3 = 0;
+int data4 = 0;
+void setup()
+{
+    Serial.begin(9600); // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+}
+
+void loop()
+{
+    uint8_t buf[12];
+    uint8_t buflen = sizeof(buf);
+    if (driver.recv(buf, &buflen)) // Non-blocking
+    {
+      int i;
+      // Message with a good checksum received, dump it.
+      //Serial.print("Message: ");
+      //Serial.println((char*)buf); 
+      data1 = buf[0];
+      data2 = buf[1];
+      data3 = buf[2];
+      data4 = buf[3]; 
+      Serial.print(data1);
+      Serial.print(" , ");
+      Serial.print(data2);
+      Serial.print(" , ");
+      Serial.print(data3);
+      Serial.print(" , ");
+      Serial.print(data4);
+      Serial.println(" ");           
+    }    
+}
---
+++ tran2final.ino
@@ -0,0 +1,37 @@
+
+#include <SPI.h>
+#include <RH_ASK.h>
+
+RH_ASK driver;
+int in1 = 6;
+int in2 = 7;
+int in3 = 8;
+int in4 = 9;
+int data1 = 0;
+int data2 = 0;
+int data3 = 0;
+int data4 = 0;
+void setup()
+{
+    Serial.begin(9600);   // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+    pinMode(in1, INPUT);
+    pinMode(in2, INPUT);
+    pinMode(in3, INPUT);
+    pinMode(in4, INPUT);
+}
+
+void loop()
+{
+    String string1 = String(digitalRead(in1), DEC);
+    String string2 = String(digitalRead(in2), DEC);
+    String string3 = String(digitalRead(in3), DEC);
+    String string4 = String(digitalRead(in4), DEC);
+    String string = string1+string2+string3+string4;
+    //char *msg = string;
+    const char * msg = string.c_str();
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(1000);
+}
