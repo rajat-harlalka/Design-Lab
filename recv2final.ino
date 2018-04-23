3823f8b85d78d0cc26fbf0c309d3d1279c4aafa1
Rajat Harlalka <rajat.harlalka2010@gmail.com>
2018-04-24 04:56:52 +0530

    Add files via upload

--- recv2final.ino
+++ recv2final.ino
@@ -1,40 +1,68 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actualy used but needed to compile
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 RH_ASK driver;
-
-int data1 = 0;
-int data2 = 0;
-int data3 = 0;
-int data4 = 0;
+//RF uses pin 11
+char data1 = '0';
+char data2 = '0';
+char data3 = '0';
+char data4 = '0';
 void setup()
 {
     Serial.begin(9600); // Debugging only
     if (!driver.init())
          Serial.println("init failed");
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    lcd.begin(16, 2);
+    lcd.print("DESIGN PROJECT");
 }
 
 void loop()
 {
-    uint8_t buf[12];
+    uint8_t buf[17];
     uint8_t buflen = sizeof(buf);
     if (driver.recv(buf, &buflen)) // Non-blocking
     {
       int i;
-      // Message with a good checksum received, dump it.
-      //Serial.print("Message: ");
-      //Serial.println((char*)buf); 
+      Serial.println((char*)buf);
+      lcd.clear();
+      lcd.print("DESIGN PROJECT");
+      lcd.setCursor(0, 1);
+      lcd.print((char*)buf);
       data1 = buf[0];
       data2 = buf[1];
       data3 = buf[2];
       data4 = buf[3]; 
-      Serial.print(data1);
+      /*Serial.print(data1);
       Serial.print(" , ");
       Serial.print(data2);
       Serial.print(" , ");
       Serial.print(data3);
       Serial.print(" , ");
       Serial.print(data4);
-      Serial.println(" ");           
-    }    
+      Serial.println(" ");*/
+      if(data1 == '1')
+        digitalWrite(8, HIGH);
+      else
+        digitalWrite(8, LOW);
+      if(data2 == '1')
+        digitalWrite(9, HIGH);
+      else
+        digitalWrite(9, LOW);
+      if(data3 == '1')
+        digitalWrite(12, HIGH);
+      else
+        digitalWrite(12, LOW);
+      if(data4 == '1')
+        digitalWrite(13, HIGH);
+      else
+        digitalWrite(13, LOW);
+                
+    }
+      
 }
--- tran2final.ino
+++ tran2final.ino
@@ -1,37 +1,52 @@
-
 #include <SPI.h>
 #include <RH_ASK.h>
 
 RH_ASK driver;
-int in1 = 6;
-int in2 = 7;
-int in3 = 8;
-int in4 = 9;
-int data1 = 0;
-int data2 = 0;
-int data3 = 0;
-int data4 = 0;
-void setup()
-{
+char data1 = '0';
+char data2 = '0';
+char data3 = '0';
+char data4 = '0';
+
+void setup() {
+  // put your setup code here, to run once:
     Serial.begin(9600);   // Debugging only
     if (!driver.init())
          Serial.println("init failed");
-    pinMode(in1, INPUT);
-    pinMode(in2, INPUT);
-    pinMode(in3, INPUT);
-    pinMode(in4, INPUT);
 }
 
-void loop()
-{
-    String string1 = String(digitalRead(in1), DEC);
-    String string2 = String(digitalRead(in2), DEC);
-    String string3 = String(digitalRead(in3), DEC);
-    String string4 = String(digitalRead(in4), DEC);
-    String string = string1+string2+string3+string4;
-    //char *msg = string;
-    const char * msg = string.c_str();
-    driver.send((uint8_t *)msg, strlen(msg));
-    driver.waitPacketSent();
-    delay(1000);
+void loop() {
+  // put your main code here, to run repeatedly:
+    if(Serial.available())
+    {
+      String data = Serial.readString();
+      Serial.println(data);
+      char char_array[data.length()+1];
+      strcpy(char_array, data.c_str()); 
+      driver.send((uint8_t *)char_array, strlen(char_array));
+      
+      data1 = char_array[0];
+      data2 = char_array[1];
+      data3 = char_array[2];
+      data4 = char_array[3];
+
+       if(data1 == '1')
+        digitalWrite(2, HIGH);
+      else
+        digitalWrite(2, LOW);
+      if(data2 == '1')
+        digitalWrite(3, HIGH);
+      else
+        digitalWrite(3, LOW);
+      if(data3 == '1')
+        digitalWrite(4, HIGH);
+      else
+        digitalWrite(4, LOW);
+      if(data4 == '1')
+        digitalWrite(5, HIGH);
+      else
+        digitalWrite(5, LOW);
+        
+      driver.waitPacketSent();
+      delay(1000);
+    }
 }
